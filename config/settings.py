8c83db0b925bf8ed14b72a158c40165ecafe0084
Anderson Dietrich Garcia Chilon <dietrichandersongarciachilon@gmail.com>
2026-02-06 17:48:17 -0500

    Fix: Configuración final de WhiteNoise para estilos del Admin

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
